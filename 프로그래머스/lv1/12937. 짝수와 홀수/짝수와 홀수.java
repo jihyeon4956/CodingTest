88e5ca83442626f7f3639b0c13f3afea49d7c5f4
jihyeon4956 <132557632+jihyeon4956@users.noreply.github.com>
2023-06-16 09:48:23 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.02 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.java
+++ 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.java
@@ -1,6 +1,9 @@
 class Solution {
-    public String solution(int num) {
-        String answer = "";      
-        return (answer%2 == 0)? "Even":"Odd";
+    public String solution(int num) {    
+        return (num % 2 == 0)? "Even":"Odd";
+    }
+    
+    public static void main (String[] args) {
+        Solution sol = new Solution();
     }
 }
