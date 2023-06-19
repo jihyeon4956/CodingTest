5a45f31727890043718d6a650c8444c624cf5afa
jihyeon4956 <132557632+jihyeon4956@users.noreply.github.com>
2023-06-20 01:14:04 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -0,0 +1,13 @@
+class Solution {
+    public long solution(long n) {
+        long answer = 0;
+        double sqrt = Math.sqrt(n);
+        
+        if(Math.sqrt(n) % 1.0 == 0) {
+            answer = Math.round(sqrt);
+            answer = (answer+1) * (answer+1);
+        } else answer = -1;
+        
+        return answer;
+    }
+}
