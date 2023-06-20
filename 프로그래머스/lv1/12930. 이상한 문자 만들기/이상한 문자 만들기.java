e19ecf4b364768c9342542d87e3ed5c9d9b228b3
jihyeon4956 <132557632+jihyeon4956@users.noreply.github.com>
2023-06-20 15:55:13 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 12.34 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,23 +1,6 @@
 class Solution {
     public String solution(String s) {
         String answer = "";
-        
-        int count = 0;
-        for(int i =0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if(ch == ' ') {
-                answer += " ";
-                count = 0;
-            }
-            else if( count % 2 == 0) {
-                answer += Character.toUpperCase(ch);
-                count++;
-            }
-            else {
-                answer += Character.toLowerCase(ch);
-                count++;
-            }
-        }
         return answer;
     }
 }
