38f9e5d2bc2c6e137e806870b5cbbda67099e046
jihyeon4956 <132557632+jihyeon4956@users.noreply.github.com>
2023-07-01 00:00:12 +0900

    [unrated] Title: 수 조작하기 1, Time: 4.07 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.java
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.java
@@ -1,25 +1,31 @@
+// class Solution {
+//     public int solution(int n, String control) {
+//         int answer = 0;
+        
+//         for (int i = 0; i < control.length(); i++) {
+//             switch (control.charAt(i)) {
+//                 case 'w':
+//                     n += 1;
+//                     break;
+//                 case 's':
+//                     n -= 1;
+//                     break;
+//                 case 'd':
+//                     n += 10;
+//                     break;
+//                 case 'a':
+//                     n -= 10;
+//                     break;
+//             }
+//         }        
+//         answer = n;
+//         return answer;
+//     }
+// }
+
+
 class Solution {
     public int solution(int n, String control) {
-        int answer = 0;
-        
-        for (int i = 0; i < control.length(); i++) {
-            switch (control.charAt(i)) {
-                case 'w':
-                    n += 1;
-                    break;
-                case 's':
-                    n -= 1;
-                    break;
-                case 'd':
-                    n += 10;
-                    break;
-                case 'a':
-                    n -= 10;
-                    break;
-            }
-        }
-        
-        answer = n;
-        return answer;
+        return control.chars().reduce(n, (acc, c) -> acc + (c == 'w' ? 1 : c == 's' ? -1 : c == 'd' ? 10 : -10));
     }
 }
