2969d66e72a6b05190046320e6510b5dbd066c24
jihyeon4956 <132557632+jihyeon4956@users.noreply.github.com>
2023-07-06 21:49:18 +0900

    [unrated] Title: rny_string, Time: 0.08 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181863. rny＿string/rny＿string.java
+++ 프로그래머스/unrated/181863. rny＿string/rny＿string.java
@@ -1,17 +1,6 @@
+
 class Solution {
     public String solution(String rny_string) {
-        StringBuilder answer = new StringBuilder();
-        
-        for (int i = 0; i < rny_string.length(); i++) {
-            char current = rny_string.charAt(i);
-            
-            if (current == 'm') {
-                answer.append("rn");
-            } else {
-                answer.append(current);
-            }
-        }
-        
-        return answer.toString();
+        return rny_string.replaceAll("m", "rn");
     }
 }
