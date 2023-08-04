efae16334ca14f26d040d141535cc8e4d1e101e6
Noh JiHyeon <132557632+jihyeon4956@users.noreply.github.com>
2023-08-04 23:20:28 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 6.56 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
+++ 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
@@ -1,21 +1,21 @@
-// class Solution {
-//     public String solution(String my_string, int n) {
-//         StringBuilder sb = new StringBuilder();
-        
-//         for (char i: my_string.toCharArray()) {
-//             sb.append(i.repeat(n));
-//         }
-        
-//         return sb.toString();
-//     }
-// }
-
 class Solution {
     public String solution(String my_string, int n) {
         StringBuilder sb = new StringBuilder();
-        for(char c : my_string.toCharArray()){
-            sb.append((c+ "").repeat(n));
+        
+        for (int i=0; i<my_string.length(); i++) {
+            sb.append((my_string.charAt(i)+"").repeat(n));
         }
+        
         return sb.toString();
     }
 }
+
+// class Solution {
+//     public String solution(String my_string, int n) {
+//         StringBuilder sb = new StringBuilder();
+//         for(char c : my_string.toCharArray()){
+//             sb.append((c+ "").repeat(n));
+//         }
+//         return sb.toString();
+//     }
+// }
