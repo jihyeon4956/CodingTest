8cb3ae7b23f8cc47a3bab063010140c1873b839a
Noh JiHyeon <132557632+jihyeon4956@users.noreply.github.com>
2023-08-05 23:47:14 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 79.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int solution(int hp) {
+        return hp / 5 + (hp % 5 / 3) + hp % 5 % 3;
+    }
+}
+
+
+// class Solution {
+//     public int solution(int hp) {
+//         int answer = 0;
+        
+//        for (int i = 0; 0 < hp; i++){
+//             if(hp % 5 == 0){
+//                 answer += hp / 5;
+//                 hp -= (hp / 5) * 5; 
+//             }else{
+//                 hp -= 3;
+//                 answer += 1;
+//             }
+//         }
+//         if(hp < 0){
+//             answer += hp+2;
+//         }
+//         return answer;
+//     }
+// }
