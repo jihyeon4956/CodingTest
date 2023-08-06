ecb496e67e66e7f803ba45bf1e7eca74c4a52610
Noh JiHyeon <132557632+jihyeon4956@users.noreply.github.com>
2023-08-06 13:43:05 +0900

    [level 0] Title: 짝수는 싫어요, Time: 1.98 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,11 @@
+import java.util.stream.IntStream;
+
+
+class Solution {
+    public int[] solution(int n) {
+
+        return IntStream.rangeClosed(0,n).filter(a -> a % 2 == 1).toArray();
+    }
+}
+
+ 
