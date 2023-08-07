cc1e6e1f7870d5cca8ae314c34aaad6c5ede749f
Noh JiHyeon <132557632+jihyeon4956@users.noreply.github.com>
2023-08-07 23:50:22 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.07 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean solution(String s) {
         return ((s.length() == 4 || s.length()==6) && s.matches("[0-9]+"));
+        // return s.matches("\\d{4}|\\d{6}");
     }
 }
