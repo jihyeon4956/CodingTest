d9dd1ae6dfbc20fcdd4715c2ffb2f02b19223ffd
Noh JiHyeon <132557632+jihyeon4956@users.noreply.github.com>
2023-08-07 23:56:10 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.08 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.java
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.java
@@ -8,3 +8,15 @@ class Solution {
         return (b - a + 1) * (a + b) / 2;
     }
 }
+
+
+// class Solution {
+
+//     public long solution(int a, int b) {
+//         return sumAtoB(Math.min(a, b), Math.max(b, a));
+//     }
+
+//     private long sumAtoB(long a, long b) {
+//         return (b - a + 1) * (a + b) / 2;
+//     }
+// }
