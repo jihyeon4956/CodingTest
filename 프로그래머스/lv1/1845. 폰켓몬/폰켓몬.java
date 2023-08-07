4369d34bc315c5315ebc54b8a3a30a44c2c77b42
Noh JiHyeon <132557632+jihyeon4956@users.noreply.github.com>
2023-08-07 23:51:01 +0900

    [level 1] Title: 폰켓몬, Time: 7.28 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -1,16 +1,29 @@
-import java.util.HashSet;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 class Solution {
     public int solution(int[] nums) {
-        int maxCount = nums.length / 2;
-        HashSet<Integer> set = new HashSet<>();
-        
-        for (int num : nums) {
-            set.add(num);
-        }
-        
-        int uniqueCount = set.size();
-        
-        return Math.min(maxCount, uniqueCount);
+        return Arrays.stream(nums)
+                .boxed()
+                .collect(Collectors.collectingAndThen(Collectors.toSet(),
+                        phonekemons -> Integer.min(phonekemons.size(), nums.length / 2)));
     }
 }
+
+
+// import java.util.HashSet;
+
+// class Solution {
+//     public int solution(int[] nums) {
+//         int maxCount = nums.length / 2;
+//         HashSet<Integer> set = new HashSet<>();
+        
+//         for (int num : nums) {
+//             set.add(num);
+//         }
+        
+//         int uniqueCount = set.size();
+        
+//         return Math.min(maxCount, uniqueCount);
+//     }
+// }
