7fc2dc7316bb0708c738dbbd407cf2b53458ed46
Noh Jihyeon <132557632+jihyeon4956@users.noreply.github.com>
2023-08-17 22:54:59 +0900

    [unrated] Title: 달리기 경주, Time: 0.03 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.java
@@ -1,62 +1,25 @@
-import java.util.Map;
 import java.util.HashMap;
 
-class Solution {
+public class Solution {
     public String[] solution(String[] players, String[] callings) {
-        Map<String, Integer> map = new HashMap<>();
+        int n = players.length;
+        HashMap<String, Integer> indexMap = new HashMap<>();
 
-        for (int i = 0; i < players.length; i++) {
-            map.put(players[i], i);
+        for (int i = 0; i < n; i++) {
+            indexMap.put(players[i], i);
         }
-        for (String player : callings) {
-            int idx = map.get(player); // 이름이 불린 선수의 인덱스를 가져옴
-            String frontPlayer = players[idx - 1]; // 이름이 불린 선수의 바로 앞에 있는 선수를 가져옴
 
-            // 이름이 불린 선수와 바로 앞에 있는 선수의 위치를 교환
-            players[idx - 1] = player;
-            players[idx] = frontPlayer;
+        for (String calling : callings) {
+            int idx = indexMap.get(calling);
+            if (idx > 0) {
+                String temp = players[idx - 1];
+                players[idx - 1] = players[idx];
+                players[idx] = temp;
 
-            // 해시맵에서 이름이 불린 선수와 바로 앞에 있는 선수의 인덱스를 갱신
-            map.put(player, idx - 1);
-            map.put(frontPlayer, idx);
+                indexMap.put(players[idx - 1], idx - 1);
+                indexMap.put(players[idx], idx);
+            }
         }
         return players;
     }
 }
-
-
-
-
-
-
-// import java.util.Map;
-// import java.util.HashMap;
-// import java.util.ArrayList;
-// import java.util.List;
-
-// class Solution {
-//     public String[] solution(String[] players, String[] callings) {
-//         Map<String, Integer> map = new HashMap<>();
-//         List<String> playerList = new ArrayList<>();
-        
-//         for (int i = 0; i < players.length; i++) {
-//             map.put(players[i], i);
-//             playerList.add(players[i]);
-//         }
-        
-//         for (String player : callings) {
-//             int idx = map.get(player);
-//             int frontIdx = idx - 1;
-            
-//             String frontPlayer = playerList.get(frontIdx);
-            
-//             playerList.set(frontIdx, player);
-//             playerList.set(idx, frontPlayer);
-            
-//             map.put(player, frontIdx);
-//             map.put(frontPlayer, idx);
-//         }
-        
-//         return playerList.toArray(new String[0]);
-//     }
-// }
