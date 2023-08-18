3d0e9eeea993969b0054b470a374bdc6051d5f30
Noh Jihyeon <132557632+jihyeon4956@users.noreply.github.com>
2023-08-18 14:56:41 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 3.38 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
+++ 프로그래머스/lv1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
@@ -1,12 +1,32 @@
+// import java.util.Arrays;
+
+// class Solution {
+//     public int[] solution(long n) {
+//         String numStr = String.valueOf(n);
+//         int[] answer = new int[numStr.length()];
+        
+//         for(int i=0; i<numStr.length(); i++) {
+//             answer[numStr.length()-i-1] = numStr.charAt(i) - '0';
+//         }
+//         return answer;
+//     }
+// }
+
+
 import java.util.Arrays;
 
 class Solution {
     public int[] solution(long n) {
-        String numStr = String.valueOf(n);
-        int[] answer = new int[numStr.length()];
-        for(int i=0; i<numStr.length(); i++) {
-            answer[numStr.length()-i-1] = numStr.charAt(i) - '0';
-        }
-        return answer;
+         return new StringBuilder(Long.toString(n))
+                // .chars() // 문자열을 스트림으로 변환
+                // .map(Character::getNumericValue) // 각 문자를 숫자로 변환
+                // .boxed() // 숫자를 래핑하여 IntStream을 Stream<Integer>로 변환
+                // .mapToInt(Integer::intValue) // Stream<Integer>를 IntStream으로 변환
+                // .toArray(); // IntStream을 int[]로 변환
+             .reverse() // 숫자를 뒤집음
+                .chars()
+                .map(Character::getNumericValue)
+                .toArray();
     }
 }
+
