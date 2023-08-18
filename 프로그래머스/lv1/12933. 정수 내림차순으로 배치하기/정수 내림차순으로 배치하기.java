cc04ceb63c7f3b21b0ae2c1e2ba8351a621dfe95
Noh Jihyeon <132557632+jihyeon4956@users.noreply.github.com>
2023-08-18 15:36:20 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.29 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
+++ 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -1,6 +1,45 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 class Solution {
     public long solution(long n) {
-        long answer = 0;
-        return answer;
+        ArrayList<Integer> temp = new ArrayList<>();
+        while(n > 0){
+            temp.add((int)(n % 10));
+            n /= 10;
+        }
+        
+        Collections.sort(temp, Collections.reverseOrder());
+        
+        StringBuilder result = new StringBuilder();
+        for (int digit : temp) {
+            result.append(digit);
+        }
+        
+        return Long.parseLong(result.toString());
     }
 }
+
+
+
+// import java.util.Arrays;
+
+// class Solution {
+//     public long solution(long n) {
+//         String str1 = Long.toString(n);
+//         char[] ch = str1.toCharArray();
+//         StringBuilder sbuilder = new StringBuilder();
+        
+//         Arrays.sort(ch);
+        
+//         for(int i=ch.length-1; i>=0; i--) {
+//             sbuilder.append(ch[i]);
+//         }
+//         String str2 = sbuilder.toString();
+//         long answer = Long.parseLong(str2);
+//         return answer;
+//     }
+// }
+
+
+
