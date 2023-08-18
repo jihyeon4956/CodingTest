7c0cca389e2b893797e4579f0202f8772cfe7f29
Noh Jihyeon <132557632+jihyeon4956@users.noreply.github.com>
2023-08-18 15:55:16 +0900

    [level 1] Title: 하샤드 수, Time: 1.50 ms, Memory: 83.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -1,28 +1,20 @@
-class Solution {
-    public boolean solution(int x) {
-        int sum =0;
-        int num = x;
-        
-        while(num != 0){
-            sum += num % 10;
-            num /= 10;
-        }
-        
-        return (x%sum==0);
-    }
-}
-
 // class Solution {
 //     public boolean solution(int x) {
-//         String numLength = String.valueOf(x);
 //         int sum =0;
-//         for(int i=0; i<numLength.length(); i++) {
-//            int a=x;
-//            while (a != 0) {
-//             sum += a % 10;
-//             a /= 10;
+//         int num = x;
+        
+//         while(num != 0){
+//             sum += num % 10;
+//             num /= 10;
 //         }
-//         }
+        
 //         return (x%sum==0);
 //     }
 // }
+
+class Solution {
+public boolean solution(int x) {
+        int sum = String.valueOf(x).chars().map(ch -> ch - '0').sum();
+        return x % sum == 0;
+    }
+}
