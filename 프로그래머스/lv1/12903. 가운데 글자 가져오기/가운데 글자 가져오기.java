d4dd0ed3680ae59121cab72ce632976593787134
Noh Jihyeon <132557632+jihyeon4956@users.noreply.github.com>
2023-08-19 15:04:27 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.02 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -1,10 +1,18 @@
 class Solution {
-    public String solution(String s) {
-        
-        int strLeng = s.length();
-        
-         if (strLeng % 2 == 1) return Character.toString(s.charAt(strLeng / 2));
-         else return s.substring(strLeng / 2 - 1, strLeng / 2 + 1);
-        
+     public String solution(String s) {
+        return s.substring((s.length()-1)/2, s.length()/2 + 1);
     }
 }
+
+
+
+// class Solution {
+//     public String solution(String s) {
+        
+//         int strLeng = s.length();
+        
+//          if (strLeng % 2 == 1) return Character.toString(s.charAt(strLeng / 2));
+//          else return s.substring(strLeng / 2 - 1, strLeng / 2 + 1);
+        
+//     }
+// }
