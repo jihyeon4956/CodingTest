5d8ce2cb200e4d495621163352e8785272e4f639
Noh Jihyeon <132557632+jihyeon4956@users.noreply.github.com>
2023-08-19 14:06:23 +0900

    [level 1] Title: 나누어 떨어지는 숫자 배열, Time: 0.57 ms, Memory: 87.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.java
+++ 프로그래머스/lv1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.java
@@ -1,51 +1,51 @@
-import java.util.ArrayList;
-import java.util.Collections;
-
-class Solution {
-    public int[] solution(int[] arr, int divisor) {
-        ArrayList<Integer> answer = new ArrayList<Integer>();
-       
-        for(int i : arr){
-            if(i % divisor == 0) {
-                answer.add(i);
-            }
-        }
-        
-        if(answer.size() == 0) {
-            answer.add(-1);
-        } else {
-           Collections.sort(answer);
-            
-        }
-        int[] intArray = answer.stream().mapToInt(Integer::intValue).toArray();
-        
-        return intArray;
-    }
-}
-
-
-
-
-// import java.util.Arrays;
+// import java.util.ArrayList;
+// import java.util.Collections;
 
 // class Solution {
 //     public int[] solution(int[] arr, int divisor) {
-//         int[] answer = new int[arr.length];
-//         int index = 0;
-        
-//         for(int i=0; i<arr.length; i++) {
-//             if(arr[i] % divisor == 0) {
-//                 answer[index] = arr[i];
-//                 index++;
+//         ArrayList<Integer> answer = new ArrayList<Integer>();
+       
+//         for(int i : arr){
+//             if(i % divisor == 0) {
+//                 answer.add(i);
 //             }
 //         }
-//         if(index == 0) {
-//             return new int[] {-1};
-//         }else {
-//             answer = Arrays.copyOf(answer, index);
-//             Arrays.sort(answer);
-//         }
         
-//         return answer;
+//         if(answer.size() == 0) {
+//             answer.add(-1);
+//         } else {
+//            Collections.sort(answer);
+            
+//         }
+//         int[] intArray = answer.stream().mapToInt(Integer::intValue).toArray();
+        
+//         return intArray;
 //     }
 // }
+
+
+
+
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] arr, int divisor) {
+        int[] answer = new int[arr.length];
+        int index = 0;
+        
+        for(int i=0; i<arr.length; i++) {
+            if(arr[i] % divisor == 0) {
+                answer[index] = arr[i];
+                index++;
+            }
+        }
+        if(index == 0) {
+            return new int[] {-1};
+        }else {
+            answer = Arrays.copyOf(answer, index);
+            Arrays.sort(answer);
+        }
+        
+        return answer;
+    }
+}
