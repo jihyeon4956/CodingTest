b1e3f3c0b5552b839007654bedca88fd1eb1aa7c
Noh Jihyeon <132557632+jihyeon4956@users.noreply.github.com>
2023-08-21 00:17:47 +0900

    [unrated] Title: 문자열로 변환, Time: 8.17 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/unrated/181845. 문자열로 변환/문자열로 변환.java
+++ 프로그래머스/unrated/181845. 문자열로 변환/문자열로 변환.java
@@ -1,6 +1,7 @@
 class Solution {
     public String solution(int n) {
-        String answer = String.valueOf(n);
+        // String answer = String.valueOf(n);
+         String answer = "" + n;
         return answer;
     }
 }
