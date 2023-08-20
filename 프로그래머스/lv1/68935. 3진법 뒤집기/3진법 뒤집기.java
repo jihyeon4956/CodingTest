2e5c736f4facdc4e3837f31271bd04f60fff217d
Noh Jihyeon <132557632+jihyeon4956@users.noreply.github.com>
2023-08-20 18:00:09 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.05 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,21 +1,6 @@
-import java.util.ArrayList;
-
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        ArrayList<Integer> tree = new ArrayList<>();
-            
-        while( n != 0) {
-            tree.add( n % 3);
-            n /= 3;
-            
-        }
-        
-         int power = 0;
-        for (int i = tree.size() - 1; i >= 0; i--) {
-            answer += tree.get(i) * Math.pow(3, power);
-            power++;
-        }
         return answer;
     }
 }
