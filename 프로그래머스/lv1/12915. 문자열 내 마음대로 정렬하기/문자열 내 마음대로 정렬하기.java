199206a01ca37c78c551ffa5bf5651708d7b01ad
Noh Jihyeon <132557632+jihyeon4956@users.noreply.github.com>
2023-08-27 21:17:48 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 9.75 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -1,18 +1,37 @@
-import java.util.Arrays;
-import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.*;
 
 class Solution {
     public String[] solution(String[] strings, int n) {
-        
-        Arrays.sort(strings, (s1,s2) -> {
-            if(s1.charAt(n) == s2.charAt(n)){
-                return s1.compareTo(s2);
-            }
-            return s1.charAt(n) - s2.charAt(n);
-        });
-            
-        
-        return strings;
+
+        ArrayList<String> arr = new ArrayList<>();
+        for(int i=0; i<strings.length; i++){
+            arr.add("" + strings[i].charAt(n) + strings[i]);
+        }
+        Collections.sort(arr);
+        String[] answer = new String[arr.size()];
+        for(int i=0; i<arr.size(); i++){
+            answer[i] = arr.get(i).substring(1, arr.get(i).length() );
+        }
+        return answer;
     }
 }
+
+
+// import java.util.Arrays;
+// import java.util.ArrayList;
+// import java.util.Comparator;
+
+// class Solution {
+//     public String[] solution(String[] strings, int n) {
+        
+//         Arrays.sort(strings, (s1,s2) -> {
+//             if(s1.charAt(n) == s2.charAt(n)){
+//                 return s1.compareTo(s2);
+//             }
+//             return s1.charAt(n) - s2.charAt(n);
+//         });
+            
+        
+//         return strings;
+//     }
+// }
