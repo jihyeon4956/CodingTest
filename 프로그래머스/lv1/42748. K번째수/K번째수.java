d7e0e330a3a82d34a11fb96468863b1f13551894
Noh Jihyeon <132557632+jihyeon4956@users.noreply.github.com>
2023-08-28 09:26:55 +0900

    [level 1] Title: K번째수, Time: 0.52 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.java
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -2,21 +2,15 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        ArrayList<Integer> answer = new ArrayList<>();
+        int[] answer = new int [commands.length];
         
         for (int i = 0; i < commands.length; i++) {
             int[] command = commands[i];
             int[] subArray = Arrays.copyOfRange(array, command[0] - 1, command[1]);
             Arrays.sort(subArray);
-            answer.add(subArray[command[2] - 1]);
+            answer[i] = subArray[command[2] - 1];
         }
         
-        // ArrayList를 int 배열로 변환
-        int[] result = new int[answer.size()];
-        for (int i = 0; i < answer.size(); i++) {
-            result[i] = answer.get(i);
-        }
-        
-        return result;
+        return answer;
     }
 }
