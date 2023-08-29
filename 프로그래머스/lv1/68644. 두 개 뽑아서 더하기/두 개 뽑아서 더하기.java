4d2c259d344d645d3b8277092ae4194539847892
Noh Jihyeon <132557632+jihyeon4956@users.noreply.github.com>
2023-08-29 09:13:59 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 1.95 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -1,24 +1,56 @@
-import java.util.*;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
 
 class Solution {
     public int[] solution(int[] numbers) {
+        Set<Integer> addSet = new TreeSet();
         
-        Set<Integer> answer = new HashSet<>();
-        
-        for(int i = 0; i < numbers.length; i++) {
-            for(int j = i + 1; j < numbers.length; j++) {  // 내부 루프 조건 수정
-                answer.add(numbers[i] + numbers[j]);  // i+j 대신 numbers[i] + numbers[j]로 수정
+        for(int i = 0; i < numbers.length-1; i++){
+            for(int j = i+1; j < numbers.length; j++){
+                addSet.add(numbers[i] + numbers[j]);
             }
         }
-        
-        List<Integer> sortedList = new ArrayList<>(answer);
-        Collections.sort(sortedList);  // 리스트 정렬
-        
-        int[] result = new int[sortedList.size()];
-        for(int i = 0; i < sortedList.size(); i++) {
-            result[i] = sortedList.get(i);
+
+        int[] answer = new int[addSet.size()];
+        int index = 0;
+        Iterator<Integer> itor = addSet.iterator();
+        while(itor.hasNext()){
+            answer[index] = (int)itor.next();
+            index++;
         }
-        
-        return result;
+
+        return answer;
     }
 }
+
+
+// import java.util.HashSet;
+// import java.util.Set;
+// import java.util.Collections;
+// import java.util.List;
+// import java.util.ArrayList;
+    
+
+// class Solution {
+//     public int[] solution(int[] numbers) {
+        
+//         Set<Integer> answer = new HashSet<>();
+        
+//         for(int i = 0; i < numbers.length; i++) {
+//             for(int j = i + 1; j < numbers.length; j++) {  // 내부 루프 조건 수정
+//                 answer.add(numbers[i] + numbers[j]);  // i+j 대신 numbers[i] + numbers[j]로 수정
+//             }
+//         }
+        
+//         List<Integer> sortedList = new ArrayList<>(answer);
+//         Collections.sort(sortedList);  // 리스트 정렬
+        
+//         int[] result = new int[sortedList.size()];
+//         for(int i = 0; i < sortedList.size(); i++) {
+//             result[i] = sortedList.get(i);
+//         }
+        
+//         return result;
+//     }
+// }
