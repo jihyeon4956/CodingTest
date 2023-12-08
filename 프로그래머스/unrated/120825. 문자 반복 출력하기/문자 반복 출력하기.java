e03cfc7bd30ee2a385eb867012ac646d00d39b0b
Noh Jihyeon <132557632+jihyeon4956@users.noreply.github.com>
2023-12-08 23:46:45 +0900

    [level unrated] Title: 문자 반복 출력하기, Time: 6.56 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120825. 문자 반복 출력하기/문자 반복 출력하기.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String solution(String my_string, int n) {
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i=0; i<my_string.length(); i++) {
+            sb.append((my_string.charAt(i)+"").repeat(n));
+        }
+        
+        return sb.toString();
+    }
+}
+
+// class Solution {
+//     public String solution(String my_string, int n) {
+//         StringBuilder sb = new StringBuilder();
+//         for(char c : my_string.toCharArray()){
+//             sb.append((c+ "").repeat(n));
+//         }
+//         return sb.toString();
+//     }
+// }
